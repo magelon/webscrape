db2bc6fc1ed99742c19dc87230d908f517c374b6
lon <alfardproject@gmail.com>
2018-01-12 23:31:51 -0800

    Add files via upload

--- scrape.py
+++ scrape.py
@@ -6,21 +6,22 @@ import json
 #data={'title':post.text}
 #sent=json.dumps(data)
 #result=firebase.post('/titles',sent)
-result = firebase.get('/userProfile',None)
+result = firebase.get('/titles',None)
 print result
 
 
 chrome_path="/Users/firoprochainezo/Desktop/chromedriver"
 driver=webdriver.Chrome(chrome_path)
-driver.get("http://vancouver.craigslist.com")
+driver.get("https://sfbay.craigslist.org")
 driver.find_element_by_xpath("""//*[@id="hhh0"]/li[1]/a""").click()
 
 posts=driver.find_elements_by_class_name("result-title")
 id=0
 for post in posts:
-    if "bedroom" in post.text:
-            print(id,post.text)
+    #if "bedroom" in post.text:
+            #print(id,post.text)
+            print(post.text)
             data={'title':post.text}
             sent=json.dumps(data)
-            firebase.post("/titles",sent)
+            #firebase.post("/titles",sent)
     
